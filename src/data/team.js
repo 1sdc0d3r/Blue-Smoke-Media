4a2e8cc489bce912dca3603065a8828935626e64
Braden <fish0859@gmail.com>
2020-08-31 08:14:32 -0600

    new images added

--- src/data/team.js
+++ src/data/team.js
@@ -1,24 +1,18 @@
-import placeholder from "../images/man-background.png";
+import placeholder from "../images/eagle.png";
 
-export default [
-  {
-    name: "Constance Beebe (CoBe)",
+export default [{
+    name: "Constance Beebe (CoBe) - Co-Founder",
     img: placeholder,
     description: "Cat Lady and UX Master",
   },
   {
-    name: "Braden Bell",
+    name: "Braden Bell - Co-Founder",
     img: placeholder,
     description: "The Master of Fullstack Web Development",
   },
   {
-    name: "AJ",
+    name: "AJ - UX Designer",
     img: placeholder,
     description: "CoBe's Apprentice",
-  },
-  {
-    name: "AR",
-    img: placeholder,
-    description: "Braden's Apprentice",
-  },
+  }
 ];
