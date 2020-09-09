469a45189d7e53180d1af6ceac2c2dfd3bb1024a
Braden Bell <fish0859@gmail.com>
2020-09-09 13:13:17 -0600

    team bio's and pricing style

--- src/data/team.js
+++ src/data/team.js
@@ -2,20 +2,26 @@ import placeholder from "../images/eagle.png";
 import AJ from "../images/Aeson-Johnson.png";
 import CoBe from "../images/CoBe.png";
 import Ramona from "../images/Ramona-Lucius.png";
-
+//todo fix 1st person/3rd person
 export default [{
     name: "Constance Beebe (CoBe) - Co-Founder",
     img: CoBe,
-    description: "Cat Lady and UX Master",
+    description: "I am a FrontEnd Web Developer & Graphic Design Artist based out of Knoxville, TN. Skilled in Search Engine Optimization (SEO) with a Google Certification in Analytics. Also skilled in Website Design, Keyword Research, Page Layout, HTML5, JavaScript, React, Node.js and CSS3. Proficient in the Adobe Creative Suite for Design. I have prior management experience from the many hotels and cabin rental companies, of which, I have had the honor of being the general manager. I am a strong engineering professional with a Certificate focused in CSS from the University of Tennessee - Knoxville and a Business Major from Charleston Southern University, in Charleston, SC.",
   },
   {
     name: "Braden Bell - Co-Founder",
     img: placeholder,
-    description: "The Master of Fullstack Web Development",
+    description: "I am an avid fisherman with a passion to develop software. The spark that was struck in my youth ignited with a roaring ball of flame. I love to help people solve their problems with a unique solution only I can provide."
+    // todo more
   },
   {
     name: "AJ - UX Designer",
     img: AJ,
-    description: "CoBe's Apprentice",
-  }
+    description: "My name is Aeson 'AJ' Johnson, and I am a Front End Web Developer skilled in UI/UX, Graphic Design, and Printed and Digital Art. I am also skilled in HTML5, CSS3, Javascript, React, and the Adobe Creative Suite for Design. I'm passionate about fresh, clean design, and always strive to make my work the best it can be. When I'm not working, you can usually find me hunched over a table playing Dungeons & Dragons with my closest friends.",
+  },
+  {
+    name: "Ramona Lucius",
+    img: Ramona,
+    description: "Ramona Lucius graduated with a BFA in Acting at Wayne State University. She holds a certificate from the Moscow Art Theatre . Ramona is the founder of Hands-On Folktales which conducts inclusive arts residencies for K-12 schools and transitional youth centers, promoting literacy throughout the nation. Most recently Ramona began a full stack web development program and started a career as an email marketer where she can use her background- combining storytelling, practices of inclusion, and technology.",
+  },
 ];
