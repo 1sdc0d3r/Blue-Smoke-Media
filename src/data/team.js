0a239275e5963cf108c294cf151b5a12012aa02a
Braden Bell <fish0859@gmail.com>
2020-09-10 17:08:27 -0600

    contact form implemented

--- src/data/team.js
+++ src/data/team.js
@@ -20,7 +20,7 @@ export default [{
     description: "My name is Aeson 'AJ' Johnson, and I am a Front End Web Developer skilled in UI/UX, Graphic Design, and Printed and Digital Art. I am also skilled in HTML5, CSS3, Javascript, React, and the Adobe Creative Suite for Design. I'm passionate about fresh, clean design, and always strive to make my work the best it can be. When I'm not working, you can usually find me hunched over a table playing Dungeons & Dragons with my closest friends.",
   },
   {
-    name: "Ramona Lucius",
+    name: "Ramona Lucius - Email Marketing",
     img: Ramona,
     description: "Ramona Lucius graduated with a BFA in Acting at Wayne State University. She holds a certificate from the Moscow Art Theatre . Ramona is the founder of Hands-On Folktales which conducts inclusive arts residencies for K-12 schools and transitional youth centers, promoting literacy throughout the nation. Most recently Ramona began a full stack web development program and started a career as an email marketer where she can use her background- combining storytelling, practices of inclusion, and technology.",
   },
