b8d96352ff9e4f51879a62b022114e35414d133c
Braden Bell <fish0859@gmail.com>
2020-10-16 14:49:13 -0600

    mobile design team image, styling

--- src/data/team.js
+++ src/data/team.js
@@ -5,12 +5,12 @@ import Daniel from '../images/daniel.png'
 import Ramona from "../images/Ramona-Lucius.png";
 
 export default [{
-    name: "Constance Beebe (CoBe) - Co-Founder",
+    name: "Constance Beebe (CoBe) - Co-Founder, Graphic Design, SEO Specialist",
     img: CoBe,
     description: "Constance Beebe is a FrontEnd Web Developer & Graphic Design Artist based out of Knoxville, TN. She is highly accomplished in Search Engine Optimization (SEO), Social Media Marketing and holds a Google Certification in Analytics. She is also skilled in Website Design, Keyword Research, Page Layout, Copywriting and is proficient in the Adobe Creative Suite for Design. She has prior management experience from the many hotels and cabin rental companies, of which she has had the honor of being the general manager. She is a strong engineering professional with a Certificate focused in CSS from the University of Tennessee - Knoxville and a Business Major from Charleston Southern University, in Charleston, SC.",
   },
   {
-    name: "Braden Bell - Co-Founder",
+    name: "Braden Bell - Co-Founder & Fullstack Developer",
     img: Braden,
     description: "I am an avid fisherman with a passion to develop software. The spark that was struck in my youth ignited with a roaring ball of flame. I love to help people solve their problems with a unique solution only I can provide."
   },
@@ -20,7 +20,7 @@ export default [{
     description: "My name is Aeson 'AJ' Johnson, and I am a Front End Web Developer skilled in UI/UX, Graphic Design, and Printed and Digital Art. I am also skilled in HTML5, CSS3, Javascript, React, and the Adobe Creative Suite for Design. I'm passionate about fresh, clean design, and always strive to make my work the best it can be. When I'm not working, you can usually find me hunched over a table playing Dungeons & Dragons with my closest friends.",
   },
   {
-    name: "Daniel Wallen - Social Sharing Specialist",
+    name: "Daniel Wallen - Sales and Social Engineer",
     img: Daniel,
     description: "Daniel Wallen is a creative copywriter with a decade of experience. His content has reached millions of people and been shared more than 500,000 times. He's also published many best-selling Amazon books for both himself and ghostwriting clients. In 2019, he took a gap year to learn code, and loves creating mobile friendly experiences. Daniel once acted in a full-length movie that had great success in film festivals and can talk his way out of anything."
   }, {
