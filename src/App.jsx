5dbea5234fa100d08fe3c622c72fe11b7f9f7517
Braden Bell <fish0859@gmail.com>
2020-10-28 16:15:04 -0600

    removed lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, lazy, Suspense } from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, useLocation, Redirect } from "react-router-dom";
 import "./style/core.css";
 
@@ -13,53 +13,31 @@ import CTA from "./components/Header-CTA";
 import DropMenu from "./components/Utils/DropMenu";
 import circleX from "./images/circle-X.png";
 import ReactGA from "react-ga";
+import Plans from "./components/Plans";
 
 // * Web Design Services
-const AdCopyWriting = lazy(() =>
-  import("./components/Web Design Services/Ad-CopyWriting")
-);
-const CustomWebsiteDesign = lazy(() =>
-  import("./components/Web Design Services/CustomWebsiteDesign")
-);
-const ECommerceWebsiteDevelopment = lazy(() =>
-  import("./components/Web Design Services/E-CommerceWebsiteDevelopment")
-);
-const InquirySurveyForm = lazy(() =>
-  import("./components/Web Design Services/InquirySurveyForm")
-);
-const WebMaintenance = lazy(() =>
-  import("./components/Web Design Services/WebMaintenance")
-);
-const WebsiteProgrammingCoding = lazy(() =>
-  import("./components/Web Design Services/WebsiteProgrammingCoding")
-);
-const WebsiteUsability = lazy(() =>
-  import("./components/Web Design Services/WebsiteUsability")
-);
+import AdCopyWriting from "./components/Web Design Services/Ad-CopyWriting";
+import CustomWebsiteDesign from "./components/Web Design Services/CustomWebsiteDesign";
+import ECommerceWebsiteDevelopment from "./components/Web Design Services/E-CommerceWebsiteDevelopment";
+import InquirySurveyForm from "./components/Web Design Services/InquirySurveyForm";
+import WebMaintenance from "./components/Web Design Services/WebMaintenance";
+import WebsiteProgrammingCoding from "./components/Web Design Services/WebsiteProgrammingCoding";
+import WebsiteUsability from "./components/Web Design Services/WebsiteUsability";
 // * SEO
-const OrganicSeo = lazy(() => import("./components/SEO/OrganicSeo"));
-const SeoFriendly = lazy(() => import("./components/SEO/SeoFriendly"));
+import OrganicSeo from "./components/SEO/OrganicSeo";
+import SeoFriendly from "./components/SEO/SeoFriendly";
 // * Graphic Design Services
-const PrintAndGraphicServices = lazy(() =>
-  import("./components/Graphic Design Services/PrintAndGraphicServices")
-);
-const PrintedMediaLogos = lazy(() =>
-  import("./components/Graphic Design Services/PrintedMediaLogos")
-);
-const WebsiteLogoDesign = lazy(() =>
-  import("./components/Graphic Design Services/WebsiteLogoDesign")
-);
-const Contact = lazy(() => import("./components/Contact"));
+import PrintAndGraphicServices from "./components/Graphic Design Services/PrintAndGraphicServices";
+import PrintedMediaLogos from "./components/Graphic Design Services/PrintedMediaLogos";
+import WebsiteLogoDesign from "./components/Graphic Design Services/WebsiteLogoDesign";
+import Contact from "./components/Contact";
 // * Media
-const PrivacyPolicy = lazy(() => import("./components/privacy"));
-const Article = lazy(() => import("./components/Article"));
-
-lazy(() => import("./style/core.css"));
+import PrivacyPolicy from "./components/privacy";
+import Article from "./components/Article";
+import "./style/core.css";
 // ! GOOGLE ANALYTICS
+import Help from "./components/Help";
 ReactGA.initialize("UA-177470204-1");
-
-const Plans = lazy(() => import("./components/Plans"));
-const Help = lazy(() => import("./components/Help"));
 
 //todo learn E-Commerce (magneto, Shopify)
 function App() {
@@ -111,62 +89,60 @@ function App() {
         </header>
         <DropMenu />
         <Switch>
-          <Suspense fallback={() => <p>Loading...</p>}>
-            <Route exact path="/" component={Home} />
-            <Route path="/team" component={Team} />
-            {/* //! privacy */}
-            <Route path="/privacy" component={PrivacyPolicy} />
-            {/* //! SEO */}
-            <Route path="/seo/Friendly" component={SeoFriendly} />
-            <Route path="/seo/seoOrganic" component={OrganicSeo} />
-            {/* //! WEBSITE DESIGN */}
-            <Route
-              path="/website-design/custom-website-design"
-              component={CustomWebsiteDesign}
-            />
-            <Route
-              path="/website-design/eCommerce-website-development"
-              component={ECommerceWebsiteDevelopment}
-            />
-            <Route
-              path="/website-design/website-useability"
-              component={WebsiteUsability}
-            />
-            <Route path="/Ad-CopyWriting" component={AdCopyWriting} />
-            <Route
-              path="/website-design/website-maintenance"
-              component={WebMaintenance}
-            />
-            <Route
-              path="/website-design/coding"
-              component={WebsiteProgrammingCoding}
-            />
-            <Route
-              path="/website-design/inquiry-form"
-              component={InquirySurveyForm}
-            />
+          <Route exact path="/" component={Home} />
+          <Route path="/team" component={Team} />
+          {/* //! privacy */}
+          <Route path="/privacy" component={PrivacyPolicy} />
+          {/* //! SEO */}
+          <Route path="/seo/Friendly" component={SeoFriendly} />
+          <Route path="/seo/seoOrganic" component={OrganicSeo} />
+          {/* //! WEBSITE DESIGN */}
+          <Route
+            path="/website-design/custom-website-design"
+            component={CustomWebsiteDesign}
+          />
+          <Route
+            path="/website-design/eCommerce-website-development"
+            component={ECommerceWebsiteDevelopment}
+          />
+          <Route
+            path="/website-design/website-useability"
+            component={WebsiteUsability}
+          />
+          <Route path="/Ad-CopyWriting" component={AdCopyWriting} />
+          <Route
+            path="/website-design/website-maintenance"
+            component={WebMaintenance}
+          />
+          <Route
+            path="/website-design/coding"
+            component={WebsiteProgrammingCoding}
+          />
+          <Route
+            path="/website-design/inquiry-form"
+            component={InquirySurveyForm}
+          />
 
-            {/* //! Graphic Design Services */}
-            <Route
-              path="/graphic-design/website-logo-design"
-              component={WebsiteLogoDesign}
-            />
-            <Route
-              path="/graphic-design/printed-media-logos"
-              component={PrintedMediaLogos}
-            />
-            <Route
-              path="/graphic-design/digital-and-print-graphic-design"
-              component={PrintAndGraphicServices}
-            />
-            <Route path="/social-media" component={SocialMedia} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/plans" component={Plans} />
-            <Route path="/pricing" component={Plans} />
-            <Route path="/help" component={Help} />
-            <Route path="/article/:id" component={Article} />
-            <Redirect to="/" />
-          </Suspense>
+          {/* //! Graphic Design Services */}
+          <Route
+            path="/graphic-design/website-logo-design"
+            component={WebsiteLogoDesign}
+          />
+          <Route
+            path="/graphic-design/printed-media-logos"
+            component={PrintedMediaLogos}
+          />
+          <Route
+            path="/graphic-design/digital-and-print-graphic-design"
+            component={PrintAndGraphicServices}
+          />
+          <Route path="/social-media" component={SocialMedia} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/plans" component={Plans} />
+          <Route path="/pricing" component={Plans} />
+          <Route path="/help" component={Help} />
+          <Route path="/article/:id" component={Article} />
+          <Redirect to="/" />
         </Switch>
 
         {/* //todo scroll to top
@@ -181,8 +157,8 @@ function App() {
 }
 
 // function getStorage() {
-//   const item = JSON.parse(localStorage.getItem("landingVideo"));
-//   // console.log(item.expiration, new Date().getTime());
+//   const item = JSON.parse(localStorage.getItem("landingVideo";
+//   // console.log(item.expiration, new Date().getTime(;
 //   if (!item || item.expiration < new Date().getTime()) {
 //     return true;
 //   }
