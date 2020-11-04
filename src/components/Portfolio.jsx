bda3624f3d66b5f25a49b790f17bd557eb74aada
Braden Bell <fish0859@gmail.com>
2020-11-04 13:00:43 -0700

    portfolio component created

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export default function Portfolio() {
+  return (
+    <>
+      <div className="portfolio">
+        <div className="nav">
+          <ul>
+            <li>Websites</li>
+            <li>E-Commerce Websites</li>
+            <li>Logos</li>
+            <li>Branding</li>
+            <li>Photo Manipulation</li>
+            <li>Videography</li>
+          </ul>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+}
