93faced510611bb2a450811faade19527f90b49d
Braden Bell <fish0859@gmail.com>
2021-04-22 11:49:36 -0600

    created all html files and set them as templates

---
+++ optimized/public/js/DropMenu.jsx
@@ -0,0 +1,161 @@
+// import { FiChevronDown } from "react-icons/fi";
+
+("use strict");
+
+class DropMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    // props.history.listen(() => {
+    //   document.getElementById("menu").classList.remove("show-drop");
+    // });
+    // const chevron = <FiChevronDown color="#f52618" />;
+  }
+
+  render() {
+    return (
+      <nav role="navigation" className="drop-menu">
+        <h2
+          onClick={() => {
+            const menu = document.getElementById("menu");
+            dropdown("menu");
+            if (menu.classList.contains("show-drop"))
+              document
+                .getElementById("services-content")
+                .classList.remove("show-drop");
+          }}
+        >
+          Menu
+        </h2>
+        <ul id="menu">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/team">Our Team</a>
+          </li>
+          <li
+            id="services"
+            className="category"
+            onClick={() => dropdown("services-content", "services")}
+          >
+            Services{"<!>"}
+          </li>
+          <ul id="services-content" className="drop-content">
+            <li>
+              <a href="/social-media">Social Media</a>
+            </li>
+            <li
+              id="seo"
+              onClick={() => dropdown("seo-content", "seo")}
+              className="category sub-category"
+            >
+              SEO{"<!>"}
+            </li>
+            <ul id="seo-content" className="drop-content sub-drop-content">
+              <li>
+                <a href="/seo/Friendly">SEO - Search Engine Optimization</a>
+              </li>
+              <li>
+                <a href="/seo/seoOrganic">Organic Search Engine Optimization</a>
+              </li>
+            </ul>
+            <li
+              id="web-design"
+              onClick={() => dropdown("web-content", "web-design")}
+              className="category sub-category"
+            >
+              Website Design{"<!>"}
+            </li>
+            <ul id="web-content" className="drop-content sub-drop-content">
+              <li>
+                <a href="/ad-CopyWriting">Website Ad-Copy Writing</a>
+              </li>
+              <li>
+                <a href="/website-design/coding">Coding and Programming</a>
+              </li>
+              <li>
+                <a href="/website-design/custom-website-design">
+                  Custom Website Design
+                </a>
+              </li>
+              <li>
+                <a href="/website-design/website-useability">
+                  Creating Your Website-Useability
+                </a>
+              </li>
+              <li>
+                <a href="/website-design/eCommerce-website-development">
+                  E-Commerce Website Development
+                </a>
+              </li>
+              <li>
+                <a href="/website-design/inquiry-form">Inquiry/Survey Form</a>
+              </li>
+              <li>
+                <a href="/website-design/website-maintenance">
+                  Website Maintenance
+                </a>
+              </li>
+            </ul>
+
+            <li
+              id="graphic-design"
+              onClick={() => dropdown("graphic-content", "graphic-design")}
+              className="category sub-category"
+            >
+              Graphic Design{"<!>"}
+            </li>
+            <ul id="graphic-content" className="drop-content sub-drop-content">
+              <li>
+                <a href="/graphic-design/website-logo-design">
+                  Website Logo Design
+                </a>
+              </li>
+              <li>
+                <a href="/graphic-design/printed-media-logos">
+                  Print Logo Design
+                </a>
+              </li>
+              <li>
+                <a href="/graphic-design/digital-and-print-graphic-design">
+                  Print & Graphic Design Services
+                </a>
+              </li>
+            </ul>
+          </ul>
+          <li>
+            <a href="/plans">Pricing</a>
+          </li>
+          <li>
+            <a
+              href="https://bluesmokemedianomads.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+const domContainer = document.querySelector("#drop-menu");
+ReactDOM.render(React.createElement(DropMenu), domContainer);
+
+function dropdown(list, active) {
+  document.getElementById(list).classList.toggle("show-drop");
+  const orange = "rgb(251, 52, 0)";
+  const blue = "#1e92f6";
+
+  if (active) {
+    const aStyle = document.getElementById(active).style;
+    aStyle.color === orange ? (aStyle.color = blue) : (aStyle.color = orange);
+  }
+}
---
+++ optimized/public/js/Footer.jsx
@@ -0,0 +1,246 @@
+("use strict");
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  // import PaymentIcon from "react-payment-icons";
+  render() {
+    const chevron = (
+      <svg
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+        data-svg="chevron-right"
+      >
+        <polyline
+          fill="none"
+          stroke="#ee3e0e"
+          strokeWidth="3.0"
+          points="7 4 13 10 7 16"
+        ></polyline>
+      </svg>
+    );
+    //* Images
+    const twitter = "../../images/Twitter.webp";
+    const linkedIn = "../../images/linkedIn_social_media_button.webp";
+    const facebook = "../../images/faceBook.webp";
+    const googleReview = "../../images/icon-Google-review.webp";
+    return (
+      <div>
+        <footer>
+          <div className="about">
+            <h3 onClick={() => dropdown(document.getElementById("drop-about"))}>
+              About Us
+            </h3>
+            <ul id="drop-about">
+              <li>
+                <a href="/">{chevron}Home</a>
+              </li>
+              <li>
+                <a href="/team">{chevron}Our Team</a>
+              </li>
+              <li>
+                <a href="/plans">{chevron}Plans & Pricing</a>
+              </li>
+              <li>
+                <a href="/help">{chevron}Need Help?</a>
+              </li>
+              <li>
+                <a href="/website-design/inquiry-form">{chevron}Inquiry Form</a>
+              </li>
+              <li>
+                <a href="/contact">{chevron}Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3
+              onClick={() => dropdown(document.getElementById("drop-product"))}
+            >
+              Products
+            </h3>
+            <ul id="drop-product">
+              <p>Website Design</p>
+              <p>
+                Search Engine
+                <br />
+                Optimization
+              </p>
+              <p>Graphic Design</p>
+              <p>Social Media & Marketing</p>
+            </ul>
+          </div>
+          <div>
+            <h3
+              onClick={() => dropdown(document.getElementById("drop-services"))}
+            >
+              Our Services
+            </h3>
+            <ul id="drop-services">
+              <li>
+                <a href="/website-design/custom-website-design">
+                  {chevron}Custom Website Design
+                </a>
+              </li>
+              <li>
+                <a href="/website-design/website-useability">
+                  {chevron}Website Useability
+                </a>
+              </li>
+              <li>
+                <a href="/website-design/eCommerce-website-development">
+                  {chevron}E-Commerce Website
+                  <br />
+                  Development
+                </a>
+              </li>
+              <li>
+                <a href="/website-design/coding">
+                  {chevron}
+                  Website Programming
+                  <br />& Development
+                </a>
+              </li>
+              <li>
+                <a href="/website-design/website-maintenance">
+                  {chevron}Website Maintenance
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3
+              onClick={() => dropdown(document.getElementById("drop-organic"))}
+            >
+              Organic SEO
+            </h3>
+            <ul id="drop-organic">
+              <li>
+                <a href="/seo/Friendly">
+                  {chevron}
+                  SEO Friendly
+                  <br />
+                  Website Design
+                </a>
+              </li>
+              <li>
+                <a href="/seo/seoOrganic">
+                  {chevron}
+                  Organic SEO
+                </a>
+              </li>
+              <li>
+                <a href="/graphic-design/digital-and-print-graphic-design">
+                  {chevron}
+                  Digital & Print
+                  <br />
+                  Graphic Design
+                </a>
+              </li>
+              <li>
+                <a href="/graphic-design/printed-media-logos">
+                  {chevron}Printed Media Logos
+                </a>
+              </li>
+              <li>
+                <a href="/graphic-design/website-logo-design">
+                  {chevron}Website Logo Design
+                </a>
+              </li>
+              <li>
+                <a href="/social-media">
+                  {chevron}
+                  Social Media & Marketing
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 onClick={() => dropdown(document.getElementById("drop-sm"))}>
+              Social Links
+            </h3>
+            <ul id="drop-sm">
+              <li>
+                <a
+                  href="https://www.linkedin.com/company/blue-smoke-digital-and-printed-media/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {chevron}
+                  <img src={linkedIn} alt="LinkedIn Social Media Button" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.facebook.com/bluesmokemedia/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {chevron}
+                  <img src={facebook} alt="Facebook Social Media Button" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://twitter.com/bluesmokemedia/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {chevron}
+                  <img src={twitter} alt="Twitter Social Media Button" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://search.google.com/local/writereview?placeid=ChIJJfs6ZcFvWogRJE1LyrS-E5Y"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {chevron}
+                  <img src={googleReview} alt="Google Social Media Button" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+        <section className="bottom">
+          <div className="payment">
+            {/* <PaymentIcon id="visa" className="payment-icon" />
+            <PaymentIcon id="paypal" className="payment-icon" />
+            <PaymentIcon id="discover" className="payment-icon" />
+            <PaymentIcon id="mastercard" className="payment-icon" /> */}
+            <p>visa</p>
+            <p>paypal</p>
+            <p>discover</p>
+            <p>mastercard</p>
+          </div>
+          <div className="contact">
+            <p>call support:</p>
+            <div className="numbers">
+              <p>
+                <a href="tel:+1.865.274.1371">+1 865.274.1371</a>
+              </p>
+              <p>
+                <a href="tel:+1.801.690.1189">+1 801.690.1189</a>
+              </p>
+            </div>
+          </div>
+        </section>
+        <p className="copy">
+          © 2019-2021 - Blue Smoke Digital and Printed Media - All rights
+          reserved
+        </p>
+      </div>
+    );
+  }
+}
+
+const domContainer = document.querySelector("#footer");
+ReactDOM.render(React.createElement(Footer), domContainer);
+
+function dropdown(evt) {
+  evt.classList.toggle("show-drop");
+  console.log(evt.classList);
+}
